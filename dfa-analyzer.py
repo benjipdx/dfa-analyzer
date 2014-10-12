429761b20cde099f7bfbca88f49471ac203a0a52
Ben Reichert <ben@benreichert.com>
2014-10-11 17:02:25 -0700

    a few hours work

---
+++ dfa-analyzer.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# ben reichert, cs311 dfa-analyzer
+
+def main():
+  print("Welcome to the DFA-Analyzer")
+  finished = False
+  while(not finished):
+    state = raw_input('Enter [1] for start state; [2] for normal state; [3] for accepting state: ')
+    state_name = raw_input('Name that state: ')
+    state_connections[]
+      connfin = False
+      while(not connfin):
+    isfinished = raw_input('Are you done entering the DFA? [y/n] ')
+
+  #end while, now prompt for example language string
+  finished = False
+  while(not finished):
+    language_string = raw_input('Enter a string to test against the DFA: ')
+    #run tests
+    isfinished = raw_input('Are you done testing strings? [y/n] ')
+
+ # name = input("What is your name? ")
+  #number = input("what is your favorite number? ")
+  #x = int(number)
+  #print("your name is", name, "number is", x)
+
+
+
+main()
